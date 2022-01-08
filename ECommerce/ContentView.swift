3542fb9f8b9726b63f346a3855f932ca036b523a
AhmedAshrafghonemy <AhmedAshrafghonemy@gmail.com>
2022-01-08 09:45:29 +0200

    Initial Commit

---
+++ ECommerce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ECommerce
+//
+//  Created by AhmedAshraf on 08/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ECommerce/ECommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceApp.swift
+//  ECommerce
+//
+//  Created by AhmedAshraf on 08/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
